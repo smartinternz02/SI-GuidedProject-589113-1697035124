feab7b8264f87bfcacce67e6884bcc9852956f64
Richik Sinha <96699739+Rickisterr@users.noreply.github.com>
2023-11-05 23:13:35 +0530

    Flask Integration of model to website

---
+++ Diabetes Prediction Model/ModelPy.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import pandas as pd
+import sklearn
+
+app = Flask(__name__, template_folder='templates')
+
+model = pickle.load(open('Diabetics_Prediction.pickle','rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+
+@app.route("/input", methods=['GET', 'POST']) 
+def predictions():
+    if request.method == 'POST': 
+        Sex = float(request.form['Sex'])
+        HighBP = float(request.form['HighBP'])
+        HighChol = float(request.form['HighChol'])
+        CholCheck = float(request.form['CholCheck'])
+        Fruits = float(request.form['Fruits'])
+        Veggies = float(request.form['Veggies'])
+        Age = float(request.form['Age'])
+        BMI = float(request.form['BMI'])
+        Smoker = float(request.form['Smoker'])
+        Stroke = float(request.form['Stroke'])
+        HvyAlcoholConsump = float(request.form['HvyAlcoholConsump'])
+        AnyHealthcare = float(request.form['AnyHealthcare'])
+        DiffWalk = float(request.form['DiffWalk'])
+        HeartDiseaseorAttack = float(request.form['HeartDiseaseorAttack'])
+        PhysActivity = float(request.form['PhysActivity'])
+        NoDocbcCost = float(request.form['NoDocbcCost'])
+        GenHlth = float(request.form['GenHlth'])
+        MentHlth = float(request.form['MentHlth'])
+        PhysHlth = float(request.form['PhysHlth'])
+        Education = float(request.form['Education'])
+        Income = float(request.form['Income'])
+        input_data = [HighBP, HighChol,	CholCheck, BMI, Smoker,	Stroke,	HeartDiseaseorAttack, PhysActivity,	Fruits,	Veggies, HvyAlcoholConsump,	AnyHealthcare, NoDocbcCost,	GenHlth, MentHlth, PhysHlth, DiffWalk, Sex,	Age, Education,	Income]
+        print(input_data)
+        print(type(input_data[0]))
+        input_data_as_nparray = np.array(input_data)
+        input_data_reshaped = input_data_as_nparray.reshape(1, -1)
+        print(input_data_reshaped)
+        prediction = model.predict(input_data_reshaped)
+        print(prediction[0])
+        if (prediction[0] == 0.0):
+            return render_template('result.html', result = 'Diabetes Absent', txtclr = 'green', name = __name__) 
+        elif (prediction[0] == 1.0):
+            return render_template('result.html', result='Diabetes Present', txtclr = 'red', name=__name__)
+    return render_template('predict.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
